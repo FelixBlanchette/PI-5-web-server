8a36d9788d2420e7d9ef1bac487a5d34147777b4
Félix Blanchette <felix.blanchette01@gmail.com>
2025-04-25 10:51:27 -0400

    implemented fallback method for 404 not found

--- server/src/main.rs
+++ server/src/main.rs
@@ -6,16 +6,30 @@ use std::{
 
 use server::ThreadPool;
 
+fn send_response(stream: &mut TcpStream, status_line: &str, body: &str) {
+    let response = format!(
+        "{status_line}\r\nContent-Lenght: {}\r\n\r\n{body}",
+        body.len()
+    );
+    stream.write_all(response.as_bytes()).unwrap();
+}
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
     let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "public/index.html")
+        ("HTTP/1.1 200 OK", "~server/src/public/index.html")
     } else {
-        ("HTTP/1.1 404 NOT FOUND", "public/404.html")
+        ("HTTP/1.1 404 NOT FOUND", "server/src/public/404.html")
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
+    let contents = match fs::read_to_string(filename) {
+        Ok(contents) => contents,
+        Err(_) => {
+            // If the file is not found, return a 404 response
+            let not_found_message = "<h1>404 - Not Found</h1>";
+            return send_response(&mut stream, "HTTP/1.1 404 NOT FOUND", not_found_message);
+        }
+    };
     let length = contents.len();
 
     let response = format!("{status_line}\r\nContent-lenght: {length}\r\n\r\n{contents}");
